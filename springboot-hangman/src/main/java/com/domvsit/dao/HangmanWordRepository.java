1539e27f1fabbde0c16392b826d49b98630f2bf5
luciomichel <lucio.michel@gmail.com>
2022-02-21 14:30:21 -0300

    ajuste de projeto

--- springboot-hangman/src/main/java/com/domvsit/dao/HangmanWordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.domvsit.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.domvsit.entities.HangmanWord;
-
-public interface HangmanWordRepository extends JpaRepository<HangmanWord, Long>{
-
-}
--- springboot-hangman/src/main/resources/static/js/env.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-
-(function(window){
-	window._env = window._env || {};
-	window._env.baseUrl
-}(this));
